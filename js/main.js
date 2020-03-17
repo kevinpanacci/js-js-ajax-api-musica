6a804ea3fcef3aa145b3a89bb506407d4fbdf82d
Kevin <kevinpanacci@hotmail.it>
2020-03-17 18:15:15 +0100

    Creato template con handlebars

--- js/main.js
+++ js/main.js
@@ -2,14 +2,28 @@
 // chiamata API: https://flynn.boolean.careers/exercises/api/array/music
 
 // Compilare il template utilizzando i dati forniti dall'API di Boolean.
-
+var source = $('#card-template').html();
+var cardTemplate = Handlebars.compile(source)
 // Chiamata all'API:
 $.ajax({
     url: "https://flynn.boolean.careers/exercises/api/array/music",
     method: "GET",
-    success: function(response) {
-        
-    }
+    success: function(valori) {
+        var valoriOggetto = valori.response;
+        console.log(valoriOggetto);
+        for (var i = 0; i < valoriOggetto.length; i++) {
+            // console.log(valoriOggetto[i]);
+            var valoriTemplate = {
+                autore: valoriOggetto[i].author,
+                genere: valoriOggetto[i].genre,
+                immagineAlbum: valoriOggetto[i].poster,
+                nomeAlbum: valoriOggetto[i].title,
+                anno: valoriOggetto[i].year
+            }
+            var template = cardTemplate(valoriTemplate);
+            $('.container-card').append(template);
+        }
+    },
 
 
 
@@ -18,8 +32,3 @@ $.ajax({
     }
 
 })
-
-
-
-var source = $('#card-template').html();
-var cardTemplate = Handlebars.compile(source)
