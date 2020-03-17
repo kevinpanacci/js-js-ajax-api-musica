ccfc1c4eae14d0ed26c671ab02b2bc50769b09e7
Kevin <kevinpanacci@hotmail.it>
2020-03-17 18:05:31 +0100

    Success:..

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+// Ricreare pagina 'spotify' come nell'esercizio visto in classe. Per motivi di tempo non ricreo html e css, ma solo JS.
+// chiamata API: https://flynn.boolean.careers/exercises/api/array/music
 
+// Compilare il template utilizzando i dati forniti dall'API di Boolean.
+
+// Chiamata all'API:
+$.ajax({
+    url: "https://flynn.boolean.careers/exercises/api/array/music",
+    method: "GET",
+    success: function(response) {
+        
+    }
+
+
+
+    error: function() {
+        alert('C\'è un errore!');
+    }
+
+})
+
+
+
+var source = $('#card-template').html();
+var cardTemplate = Handlebars.compile(source)
